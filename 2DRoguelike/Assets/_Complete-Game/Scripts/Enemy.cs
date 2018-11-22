c7ec8b30c5fe5fbd5287786216cbb73ecbfa7844
shimada15 <iukfsafczn@i.softbank.jp>
2018-11-22 09:30:40 +0900

    作業コピー

--- 2DRoguelike/Assets/_Complete-Game/Scripts/Enemy.cs
+++ 2DRoguelike/Assets/_Complete-Game/Scripts/Enemy.cs
@@ -3,15 +3,14 @@ using System.Collections;
 
 namespace Completed
 {
-	//Enemy inherits from MovingObject, our base class for objects that can move, Player also inherits from this.
-	public class Enemy : MovingObject
+    //敵は移動可能なオブジェクトの基本クラスであるMovingObjectから継承し、Playerはこれを継承します。
+    public class Enemy : MovingObject
 	{
-		public int playerDamage; 							//The amount of food points to subtract from the player when attacking.
-		public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
-		public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
-		
-		
-		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
+        public int playerDamage;                            //攻撃時にプレイヤーから減算するフードポイントの量。
+        public AudioClip attackSound1;                      //プレーヤーを攻撃するときに再生する2つのオーディオクリップのうち最初のもの。
+        public AudioClip attackSound2;                      //プレーヤーを攻撃するときに再生する2つのオーディオクリップの秒。
+
+        private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
 		private Transform target;							//Transform to attempt to move toward each turn.
 		private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
 		
--- 2DRoguelike/Assets/_Complete-Game/Scripts/Player.cs
+++ 2DRoguelike/Assets/_Complete-Game/Scripts/Player.cs
@@ -5,261 +5,261 @@ using UnityEngine.SceneManagement;
 
 namespace Completed
 {
-	//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
-	public class Player : MovingObject
+    //Playerは移動できるオブジェクトの基本クラスであるMovingObjectから継承し、Enemyはこれを継承します。
+    public class Player : MovingObject
 	{
-		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
-		public int pointsPerFood = 10;				//Number of points to add to player food points when picking up a food object.
-		public int pointsPerSoda = 20;				//Number of points to add to player food points when picking up a soda object.
-		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
-		public Text foodText;						//UI Text to display current player food total.
-		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
-		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
-		public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
-		public AudioClip eatSound2;					//2 of 2 Audio clips to play when player collects a food object.
-		public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
-		public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
-		public AudioClip gameOverSound;				//Audio clip to play when player dies.
-		
-		private Animator animator;					//Used to store a reference to the Player's animator component.
-		private int food;                           //Used to store player food points total during level.
+		public float restartLevelDelay = 1f;        //レベルを再開するまでの遅延時間（秒単位）。
+        public int pointsPerFood = 10;              //食べ物を拾うときにプレイヤーの食べ物ポイントに追加するポイントの数。
+        public int pointsPerSoda = 20;              //ソーダオブジェクトをピックアップするときにプレイヤーの食べ物ポイントに追加するポイントの数。
+        public int wallDamage = 1;					//それをチョッピングするときにプレイヤーが壁に与えるダメージ。
+		public Text foodText;                       //現在のプレーヤーの食べ物の合計を表示するUIテキスト。
+        public AudioClip moveSound1;                //1 of 2プレイヤーが動いたときに再生するオーディオクリップ。
+        public AudioClip moveSound2;                //2 of 2プレイヤーが動いたときに再生するオーディオクリップ。
+        public AudioClip eatSound1;                 //プレイヤーが食品オブジェクトを収集するときに再生するオーディオクリップ。
+        public AudioClip eatSound2;                 //2 of 2プレイヤーが食品オブジェクトを収集するときに再生するオーディオクリップ。
+        public AudioClip drinkSound1;               //1 of 2プレイヤーがソーダオブジェクトを収集するときに再生するオーディオクリップ。
+        public AudioClip drinkSound2;               //2 of 2プレイヤーがソーダオブジェクトを収集するときに再生するオーディオクリップ。
+        public AudioClip gameOverSound;             //プレーヤーが死亡したときに再生するオーディオクリップ.
+
+        private Animator animator;                  //Playerのアニメータコンポーネントへの参照を格納するために使用します。
+        private int food;                           //レベル中にプレイヤーの食べ物ポイント合計を格納するために使用されます。
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
 #endif
-		
-		
-		//Start overrides the Start function of MovingObject
-		protected override void Start ()
+
+
+        //StartはMovingObjectのStart関数をオーバーライドします。
+        protected override void Start ()
 		{
-			//Get a component reference to the Player's animator component
-			animator = GetComponent<Animator>();
-			
-			//Get the current food point total stored in GameManager.instance between levels.
-			food = GameManager.instance.playerFoodPoints;
-			
-			//Set the foodText to reflect the current player food total.
-			foodText.text = "Food: " + food;
-			
-			//Call the Start function of the MovingObject base class.
-			base.Start ();
+            //Playerのアニメータコンポーネントへのコンポーネント参照を取得する
+            animator = GetComponent<Animator>();
+
+            //レベル間でGameManager.instanceに保存されている現在の食品ポイント合計を取得します。
+            food = GameManager.instance.playerFoodPoints;
+
+            //現在のプレイヤーの食べ物の合計を反映するようにfoodTextを設定します。
+            foodText.text = "Food: " + food;
+
+            //MovingObject基本クラスのStart関数を呼び出します。
+            base.Start ();
 		}
-		
-		
-		//This function is called when the behaviour becomes disabled or inactive.
-		private void OnDisable ()
+
+
+        //この関数は、ビヘイビアが無効または無効になったときに呼び出されます。
+        private void OnDisable ()
 		{
-			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
-			GameManager.instance.playerFoodPoints = food;
+            //Playerオブジェクトが無効になっているときは、現在のローカルフードの合計をGameManagerに格納して、次のレベルに再ロードすることができます。
+            GameManager.instance.playerFoodPoints = food;
 		}
 		
 		
 		private void Update ()
 		{
-			//If it's not the player's turn, exit the function.
-			if(!GameManager.instance.playersTurn) return;
+            //プレイヤーのターンでない場合は、その機能を終了します。
+            if (!GameManager.instance.playersTurn) return;
 			
-			int horizontal = 0;  	//Used to store the horizontal move direction.
-			int vertical = 0;		//Used to store the vertical move direction.
-			
-			//Check if we are running either in the Unity editor or in a standalone build.
+			int horizontal = 0;     //水平移動方向を格納するために使用します。
+            int vertical = 0;       //垂直移動方向を格納するために使用します。
+
+            //Unityエディタまたはスタンドアロンビルドで実行しているかどうかを確認します。
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
-			
-			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
-			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
-			
-			//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
-			vertical = (int) (Input.GetAxisRaw ("Vertical"));
-			
-			//Check if moving horizontally, if so set vertical to zero.
-			if(horizontal != 0)
+
+            //入力マネージャから入力を取得し、整数に丸め、水平軸に格納してx軸の移動方向を設定します
+            horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+
+            //入力マネージャから入力を取得し、整数に丸め、y軸の移動方向を設定するために垂直に格納する
+            vertical = (int) (Input.GetAxisRaw ("Vertical"));
+
+            //水平方向に移動するかどうかを確認し、垂直方向にゼロに設定する。
+            if (horizontal != 0)
 			{
 				vertical = 0;
 			}
-			//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+            //私たちがiOS、Android、Windows Phone 8、またはUnity iPhoneで動作しているか確認してください
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 			
-			//Check if Input has registered more than zero touches
+			//入力がゼロ以上のタッチを登録しているかどうかを確認する
 			if (Input.touchCount > 0)
 			{
-				//Store the first touch detected.
+				//最初に検出されたタッチを保存します。
 				Touch myTouch = Input.touches[0];
 				
-				//Check if the phase of that touch equals Began
+				//そのタッチの位相がBeganに等しいかどうかを確認する
 				if (myTouch.phase == TouchPhase.Began)
 				{
-					//If so, set touchOrigin to the position of that touch
+					//その場合は、touchOriginをそのタッチの位置に設定します
 					touchOrigin = myTouch.position;
 				}
 				
-				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+				//タッチ位相がBeganでなく、代わりにEndedに等しく、touchOriginのxがゼロ以上である場合：
 				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
 				{
-					//Set touchEnd to equal the position of this touch
+					//このタッチの位置と等しくなるようにtouchEndを設定します
 					Vector2 touchEnd = myTouch.position;
 					
-					//Calculate the difference between the beginning and end of the touch on the x axis.
+					//x軸のタッチの開始と終了の差を計算します。
 					float x = touchEnd.x - touchOrigin.x;
 					
-					//Calculate the difference between the beginning and end of the touch on the y axis.
+					//タッチの開始と終了の差をy軸上で計算します。
 					float y = touchEnd.y - touchOrigin.y;
 					
-					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+					//touchIrigin.xを-1に設定すると、else if文がfalseと評価され、直ちに繰り返されることはありません。
 					touchOrigin.x = -1;
 					
-					//Check if the difference along the x axis is greater than the difference along the y axis.
+					//x軸の差がy軸の差より大きいかどうかを確認してください。
 					if (Mathf.Abs(x) > Mathf.Abs(y))
-						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+						//xが0より大きい場合、horizontalを1に設定し、そうでない場合は-1に設定します
 						horizontal = x > 0 ? 1 : -1;
 					else
-						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+						//yが0より大きい場合は、horizontalを1に設定し、そうでない場合は-1に設定します
 						vertical = y > 0 ? 1 : -1;
 				}
 			}
 			
-#endif //End of mobile platform dependendent compilation section started above with #elif
-			//Check if we have a non-zero value for horizontal or vertical
-			if(horizontal != 0 || vertical != 0)
+#endif //#elifで上で起動したモバイルプラットフォーム依存のコンパイルセクションの終わり
+            //水平または垂直に0以外の値があるかどうかを確認する
+            if (horizontal != 0 || vertical != 0)
 			{
-				//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
-				//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
-				AttemptMove<Wall> (horizontal, vertical);
+                //AttemptMoveがジェネリックパラメータWallを渡すのは、プレイヤーが攻撃に遭遇した場合（攻撃した場合）
+                //パラメータとして水平と垂直を渡して、Playerを移動する方向を指定します。
+                AttemptMove<Wall> (horizontal, vertical);
 			}
 		}
-		
-		//AttemptMove overrides the AttemptMove function in the base class MovingObject
-		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
-		protected override void AttemptMove <T> (int xDir, int yDir)
+
+        //AttemptMoveは、基本クラスMovingObjectのAttemptMove関数をオーバーライドします。
+        //AttemptMoveはジェネリックパラメータTを取ります。これは、PlayerがWall型で、x方向とy方向が移動するための整数も取ります。
+        protected override void AttemptMove <T> (int xDir, int yDir)
 		{
-			//Every time player moves, subtract from food points total.
-			food--;
-			
-			//Update food text display to reflect current score.
-			foodText.text = "Food: " + food;
-			
-			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
-			base.AttemptMove <T> (xDir, yDir);
-			
-			//Hit allows us to reference the result of the Linecast done in Move.
-			RaycastHit2D hit;
-			
-			//If Move returns true, meaning Player was able to move into an empty space.
-			if (Move (xDir, yDir, out hit)) 
+            //プレイヤーが移動するたびに、合計食糧ポイントを引く。
+            food--;
+
+            //現在のスコアを反映するように食べ物のテキスト表示を更新します。
+            foodText.text = "Food: " + food;
+
+            //基本クラスのAttemptMoveメソッドを呼び出し、コンポーネントT（この場合はWall）とx方向とy方向を移動して渡します。
+            base.AttemptMove <T> (xDir, yDir);
+
+            //ヒットは、我々が移動で行なわれたラインキャストの結果を参照することを可能にする。
+            RaycastHit2D hit;
+
+            //Moveがtrueを返した場合、Playerは空の領域に移動できました。
+            if (Move (xDir, yDir, out hit)) 
 			{
-				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
-				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+                //SoundManagerのRandomizeSfxを呼び出して、2つのオーディオクリップを渡して移動音を再生します。
+                SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
 			}
-			
-			//Since the player has moved and lost food points, check if the game has ended.
-			CheckIfGameOver ();
-			
-			//Set the playersTurn boolean of GameManager to false now that players turn is over.
-			GameManager.instance.playersTurn = false;
+
+            //プレイヤーは移動して食糧を失ったため、ゲームが終了したかどうかを確認します。
+            CheckIfGameOver();
+
+            //プレイヤーが終了したら、GameManagerのplayersTurn booleanをfalseに設定します。
+            GameManager.instance.playersTurn = false;
 		}
-		
-		
-		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
-		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
-		protected override void OnCantMove <T> (T component)
+
+
+        //OnCantMoveは、MovingObjectのOnCantMoveという抽象関数をオーバーライドします。
+        //それはプレーヤーが攻撃で破壊することができる壁である場合、一般的なパラメータTをとります。
+        protected override void OnCantMove <T> (T component)
 		{
-			//Set hitWall to equal the component passed in as a parameter.
-			Wall hitWall = component as Wall;
-			
-			//Call the DamageWall function of the Wall we are hitting.
-			hitWall.DamageWall (wallDamage);
-			
-			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
-			animator.SetTrigger ("playerChop");
+            //パラメーターとして渡されたコンポーネントと等しくなるようにhitWallを設定します。
+            Wall hitWall = component as Wall;
+
+            //私たちが襲っている壁のDamageWall機能を呼び出します。
+            hitWall.DamageWall (wallDamage);
+
+            //プレイヤーの攻撃アニメーションを再生するために、プレイヤーのアニメーションコントローラーの攻撃トリガーを設定します。
+            animator.SetTrigger ("playerChop");
 		}
-		
-		
-		//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
-		private void OnTriggerEnter2D (Collider2D other)
+
+
+        //OnTriggerEnter2Dは、別のオブジェクトがこのオブジェクトに接続されたトリガーコライダーに入ると送信されます（2D物理のみ）。
+        private void OnTriggerEnter2D (Collider2D other)
 		{
-			//Check if the tag of the trigger collided with is Exit.
-			if(other.tag == "Exit")
+            //衝突したトリガーのタグがExitかどうかを確認してください。
+            if (other.tag == "Exit")
 			{
-				//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
-				Invoke ("Restart", restartLevelDelay);
-				
-				//Disable the player object since level is over.
-				enabled = false;
+                //restartLevelDelayの遅延（デフォルトは1秒）で次のレベルを開始するには、Restart関数を呼び出します。
+                Invoke("Restart", restartLevelDelay);
+
+                //レベルが終わったのでプレーヤーオブジェクトを無効にします。
+                enabled = false;
 			}
-			
-			//Check if the tag of the trigger collided with is Food.
-			else if(other.tag == "Food")
+
+            //衝突したトリガーのタグがFoodかどうかを確認します。
+            else if (other.tag == "Food")
 			{
-				//Add pointsPerFood to the players current food total.
-				food += pointsPerFood;
-				
-				//Update foodText to represent current total and notify player that they gained points
-				foodText.text = "+" + pointsPerFood + " Food: " + food;
-				
-				//Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
-				SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
-				
-				//Disable the food object the player collided with.
-				other.gameObject.SetActive (false);
+                //選手の現在の食べ物の合計にpointsPerFoodを追加します。
+                food += pointsPerFood;
+
+                //foodTextを更新して現在の合計を表し、ポイントを得たことをプレーヤーに通知します
+                foodText.text = "+" + pointsPerFood + " Food: " + food;
+
+                //oundManagerのRandomizeSfx関数を呼び出し、2つの食べ物の音を渡して、食べるサウンドエフェクトを再生するかどうかを選択します。
+                SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
+
+                //プレイヤーが衝突した食物を無効にする。
+                other.gameObject.SetActive (false);
 			}
-			
-			//Check if the tag of the trigger collided with is Soda.
-			else if(other.tag == "Soda")
+
+            //衝突したトリガーのタグがソーダかどうかを確認します。
+            else if (other.tag == "Soda")
 			{
-				//Add pointsPerSoda to players food points total
-				food += pointsPerSoda;
-				
-				//Update foodText to represent current total and notify player that they gained points
-				foodText.text = "+" + pointsPerSoda + " Food: " + food;
-				
-				//Call the RandomizeSfx function of SoundManager and pass in two drinking sounds to choose between to play the drinking sound effect.
-				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
-				
-				//Disable the soda object the player collided with.
-				other.gameObject.SetActive (false);
+                //プレイヤーにポイントを追加する
+                food += pointsPerSoda;
+
+                //foodTextを更新して現在の合計を表し、ポイントを得たことをプレーヤーに通知します
+                foodText.text = "+" + pointsPerSoda + " Food: " + food;
+
+                //SoundManagerのRandomizeSfx関数を呼び出し、2つの飲酒音を渡して、飲酒音の効果を再生するかどうかを選択します。
+                SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+
+                //プレーヤーが衝突したソーダオブジェクトを無効にする。
+                other.gameObject.SetActive (false);
 			}
 		}
-		
-		
-		//Restart reloads the scene when called.
-		private void Restart ()
+
+
+        //再起動すると、呼び出されるとシーンがリロードされます。
+        private void Restart ()
 		{
-			//Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
-            //and not load all the scene object in the current scene.
+            //ロードされた最後のシーン、この場合Mainをゲームの唯一のシーンにロードします。 そして、それを "シングル"モードでロードして、既存のものを置き換えます
+            //現在のシーン内のすべてのシーンオブジェクトをロードしません。
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
 		}
-		
-		
-		//LoseFood is called when an enemy attacks the player.
-		//It takes a parameter loss which specifies how many points to lose.
-		public void LoseFood (int loss)
+
+
+        //LoseFoodは、敵がプレイヤーを攻撃するときに呼び出されます。
+        //失うポイント数を指定するパラメータの損失が発生します。
+        public void LoseFood (int loss)
 		{
-			//Set the trigger for the player animator to transition to the playerHit animation.
-			animator.SetTrigger ("playerHit");
-			
-			//Subtract lost food points from the players total.
-			food -= loss;
-			
-			//Update the food display with the new total.
-			foodText.text = "-"+ loss + " Food: " + food;
-			
-			//Check to see if game has ended.
-			CheckIfGameOver ();
+            //playerHitアニメーションに遷移するように、プレーヤアニメータのトリガを設定します。
+            animator.SetTrigger ("playerHit");
+
+            //プレイヤーの合計から失われた食べ物ポイントを差し引く。
+            food -= loss;
+
+            //フードディスプレイを新しい合計で更新します。
+            foodText.text = "-"+ loss + " Food: " + food;
+
+            //ゲームが終了したかどうかを確認してください。
+            CheckIfGameOver();
 		}
-		
-		
-		//CheckIfGameOver checks if the player is out of food points and if so, ends the game.
-		private void CheckIfGameOver ()
+
+
+        //CheckIfGameOverはプレイヤーに食糧ポイントがないかどうかをチェックし、プレイヤーがフードポイントを使い果たしていない場合はゲームを終了します。
+        private void CheckIfGameOver ()
 		{
-			//Check if food point total is less than or equal to zero.
-			if (food <= 0) 
+            //フードポイント合計がゼロ以下であることを確認します。
+            if (food <= 0) 
 			{
-				//Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
-				SoundManager.instance.PlaySingle (gameOverSound);
-				
-				//Stop the background music.
-				SoundManager.instance.musicSource.Stop();
-				
-				//Call the GameOver function of GameManager.
-				GameManager.instance.GameOver ();
+                //SoundManagerのPlaySingle関数を呼び出し、再生するオーディオクリップとしてgameOverSoundを渡します。
+                SoundManager.instance.PlaySingle (gameOverSound);
+
+                //バックグラウンドミュージックを停止します。
+                SoundManager.instance.musicSource.Stop();
+
+                //GameManagerのGameOver関数を呼び出します。
+                GameManager.instance.GameOver ();
 			}
 		}
 	}
